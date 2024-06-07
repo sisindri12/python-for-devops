f535c6b132c87472b49ae7645a7ae71798f8a299
sisindri12 <59793688+sisindri12@users.noreply.github.com>
2024-06-07 07:41:37 +0000

    examples

---
+++ Day-06/02-Assignment/identityop.py
@@ -0,0 +1,9 @@
+a = 23
+b = 44
+
+c = a is b
+d = a is not b
+
+print("the statement c is:", c)
+print("the statement d is:", d)
+
---
+++ Day-06/02-Assignment/memberop.py
@@ -0,0 +1,7 @@
+my_list = ['apple', 'banana', 'grapes']
+a ='apples' in my_list
+b = 'grapes' not in my_list
+
+print("in result:", a)
+print("not in result:", b)
+
